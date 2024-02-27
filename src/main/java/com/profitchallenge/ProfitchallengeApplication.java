cb763aa7cac0607e1547731750a5d43265a86f96
hyungjae.lee <dlgudwo11@gmail.com>
2024-02-27 13:30:30 +0900

    Initial commit

---
+++ src/main/java/com/profitchallenge/ProfitchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.profitchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitchallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfitchallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/profitchallenge/ProfitchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profitchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfitchallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
